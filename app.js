390883537bbd6f56927ad5379d06d01dd2fc4f42
Aaron Sweeney <aaron.sweeney@ucdconnect.ie>
2016-03-04 21:50:50 +0000

    display loading image until data is loaded

--- app.js
+++ app.js
@@ -3,3 +3,11 @@ var myApp = angular.module("myApp",[]);
 myApp.controller("DataController", ["$scope", "$http", "$location",
   function($scope, $http, $location) {
 }]);
+
+
+
+$scope.loaded = false;
+$http.get('https://api.myjson.com/bins/54jf7').success(function(data){
+  $scope.movies = data;
+  $scope.loaded = true;
+});
